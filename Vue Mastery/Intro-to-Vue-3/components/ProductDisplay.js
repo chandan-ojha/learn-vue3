1a4028d5a52519af98ea067022bb3a799bc192eb
Chandan Ojha <chandanojha776@gmail.com>
2022-10-31 11:00:01 +0600

    components & props practice done

---
+++ Vue Mastery/Intro-to-Vue-3/components/ProductDisplay.js
@@ -0,0 +1,94 @@
+app.component('product-display',{
+    props: {
+        premimum: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `<div class="product-display">
+         <div class="product-container">
+             <div class="product-image">
+                     <img :src="image">
+                 <!--<img :class="{ 'out-of-stock-img': !inStock }" v-bind:src="image">-->
+             </div>
+             <div class="product-info">
+                 <h1>{{ title }}</h1>
+                 <!--<a :href="url">Made by Vue Mastery</a>-->
+                 <!--<p v-if="inventory > 10">In Stock</p>
+                 <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+                 <p v-else>Out of Stock</p>-->
+                 <p v-if="inStock">In Stock</p>
+                 <p v-else>Out of Stock</p>
+                 <p>Shipping: {{ shipping }}</p>
+                 <!--<ul>
+                     <li v-for="detail in details">{{detail}}</li>
+                 </ul>-->
+                 <product-details :details="details"></product-details>
+                 <!--<ul>
+                     <li v-for="(size, index) in sizes" :key="index">{{ size }}</li>
+                 </ul>-->
+                 <div
+                         v-for="(variant,index) in variants"
+                         :key="variant.id"
+                         @mouseover="updateVariant(index)"
+                         class="color-circle"
+                         :style="{ backgroundColor: variant.color }">
+                 </div>
+                 <button
+                         class="button"
+                         :class="{ disabledButton: !inStock }"
+                         :disabled="!inStock"
+                         @click="addToCart">
+                     Add to Cart
+                 </button>
+                 <button class="button" @click="removeFromCart">Remove Item</button>
+             </div>
+         </div>
+      </div>`,
+    data(){
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton','30% wool','20% polyester'],
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+                {id: 2234, color:'green', image: './assets/images/socks_green.jpg',quantity:50},
+                {id: 2235, color:'blue', image: './assets/images/socks_blue.jpg',quantity:0},
+            ]
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateVariant(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        shipping(){
+            if(this.premimum){
+                return 'Free'
+            }
+            return 2.99
+        }
+    }
+
+})
--- Vue Mastery/Intro-to-Vue-3/main.js
+++ Vue Mastery/Intro-to-Vue-3/main.js
@@ -2,39 +2,8 @@ const app = Vue.createApp({
     data(){
         return {
             cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            details: ['50% cotton','30% wool','20% polyester'],
-            sizes: ['S', 'M', 'L', 'XL'],
-            variants: [
-                {id: 2234, color:'green', image: './assets/images/socks_green.jpg',quantity:50},
-                {id: 2235, color:'blue', image: './assets/images/socks_blue.jpg',quantity:0},
-            ]
+            premimum: false
         }
     },
-    methods:{
-        addToCart(){
-            this.cart += 1;
-        },
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        updateVariant(index){
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title(){
-            return this.brand + ' ' + this.product
-        },
-        image(){
-            return this.variants[this.selectedVariant].image
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity
-        }
-    }
+    methods: {}
 })
