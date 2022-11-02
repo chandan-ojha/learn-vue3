5110e096c1bcb2cabb57a22d38efba1404820593
Chandan Ojha <chandanojha776@gmail.com>
2022-11-02 09:59:31 +0600

    handle a form submission

--- Laracasts Vue-3 Course/Event Handling/js/components/Assignments.js
+++ Laracasts Vue-3 Course/Event Handling/js/components/Assignments.js
@@ -7,6 +7,14 @@ export default {
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                  <input v-model="newAssignment" placeholder="New assignment..." class="p-2">
+                  <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
+            
         </section>
     `,
 
@@ -16,7 +24,8 @@ export default {
                 { name: 'Finish project', complete: false, id: 1 },
                 { name: 'Read Chapter 4', complete: false, id: 2 },
                 { name: 'Turn in Homework', complete: false, id: 3 },
-            ]
+            ],
+            newAssignment: ''
         }
     },
 
@@ -26,6 +35,19 @@ export default {
                 inProgress: this.assignments.filter(assignment => ! assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete)
             };
+        }
+    },
+    methods : {
+        add(){
+            //e.preventDefault();
+            //alert('Hi there');
+            //alert(this.newAssignment)
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1
+            });
+            this.newAssignment = '';
         }
     }
 }
