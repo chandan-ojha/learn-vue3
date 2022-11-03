e49156d2a42939c5e134fdb120b1b6bec599e590
Chandan Ojha <chandanojha776@gmail.com>
2022-11-03 10:22:18 +0600

    component responsibility practice

--- Laracasts Vue-3 Course/Event Handling/js/components/AssignmentList.js
+++ Laracasts Vue-3 Course/Event Handling/js/components/AssignmentList.js
@@ -1,7 +1,8 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-    components: { Assignment },
+    components: { Assignment, AssignmentTags },
 
     template:
         `<section v-show="assignments.length">
@@ -9,14 +10,13 @@ export default {
                 {{ title }}
                 <span>({{ assignments.length }})</span>
             </h2>
-            <div class="flex gap-2">
-                <button 
-                   @click="currentTag = tag"
-                   v-for="tag in tags" 
-                   class="border rounded px-1 py-px text-xs"
-                   :class ="{ 'border-blue-500 text-blue-500': tag === currentTag }"
-                   > {{ tag }} </button>
-            </div>
+            
+            <assignment-tags 
+                :initial-tags="assignments.map(a => a.tag)"
+                :current-tag="currentTag"
+                @change = "currentTag = $event"
+            />
+            
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                <assignment 
                     v-for="assignment in filteredAssignments"
@@ -43,9 +43,6 @@ export default {
                 return this.assignments;
             }
             return this.assignments.filter(a => a.tag === this.currentTag);
-        },
-        tags(){
-            return ['all',...new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
