12b0731f6fad69cc097b506c0428ebc6388701de
Chandan Ojha <chandanojha776@gmail.com>
2022-11-14 15:52:46 +0600

    practice done
    Refactor to defineProps and defineEmits

--- Laracasts Vue-3 Course/my-first-vue-project/src/views/HomeView.vue
+++ Laracasts Vue-3 Course/my-first-vue-project/src/views/HomeView.vue
@@ -106,13 +106,15 @@ setTimeout(()=> {
 
 <!--Refactor to defineProps and defineEmits-->
 <script setup>
-
+import TabbableTextarea from "@/components/TabbableTextarea.vue";
+import { ref } from "vue";
+let comment = ref('initial textarea value');
 </script>
 
 <template>
   <main>
     <form>
-       <textarea style="width: 100%; height: 300px;">Hi,There</textarea>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;" />
     </form>
   </main>
 </template>
