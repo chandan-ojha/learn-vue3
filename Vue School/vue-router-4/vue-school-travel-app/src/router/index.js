5cbe5f75284a84d849cca3550d6196e8156f813e
Chandan Ojha <chandanojha776@gmail.com>
2023-02-18 14:24:13 +0600

    Nested Routes

--- Vue School/vue-router-4/vue-school-travel-app/src/router/index.js
+++ Vue School/vue-router-4/vue-school-travel-app/src/router/index.js
@@ -15,13 +15,17 @@ const router = createRouter({
       name: "destination.show",
       component: () => import("../views/DestinationShow.vue"),
       props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
-    },
-
-    {
-      path: "/destination/:id/:slug/:experienceSlug",
-      name: "experience.show",
-      component: () => import("../views/ExperienceShow.vue"),
-      props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      children: [
+        {
+          path: ":experienceSlug",
+          name: "experience.show",
+          component: () => import("../views/ExperienceShow.vue"),
+          props: (route) => ({
+            ...route.params,
+            id: parseInt(route.params.id),
+          }),
+        },
+      ],
     },
   ],
 });
--- Vue School/vue-router-4/vue-school-travel-app/src/views/DestinationShow.vue
+++ Vue School/vue-router-4/vue-school-travel-app/src/views/DestinationShow.vue
@@ -21,6 +21,7 @@
         <ExperienceCard :experience="experience" />
       </router-link>
     </div>
+    <router-view />
   </section>
 </template>
 
