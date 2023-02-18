6b82f34f0d72342f9ac3f1fe72e8cd029618a14c
Chandan Ojha <chandanojha776@gmail.com>
2023-02-18 19:29:27 +0600

    Go Back

--- Vue School/vue-router-4/vue-school-travel-app/src/views/DestinationShow.vue
+++ Vue School/vue-router-4/vue-school-travel-app/src/views/DestinationShow.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="destination">
     <h1>{{ destination.name }}</h1>
+    <GoBack />
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name" />
       <p>{{ destination.description }}</p>
@@ -28,9 +29,10 @@
 <script>
 import sourceData from "@/data.json";
 import ExperienceCard from "@/components/ExperienceCard.vue";
+import GoBack from "@/components/GoBack.vue";
 
 export default {
-  components: { ExperienceCard },
+  components: { ExperienceCard, GoBack },
   props: {
     id: { type: Number, required: true },
   },
