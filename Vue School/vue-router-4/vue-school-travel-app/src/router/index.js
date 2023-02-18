4d098b520119038630002fc2c89fcf84abe5c0ab
Chandan Ojha <chandanojha776@gmail.com>
2023-02-18 22:07:37 +0600

    Navigation Guards

--- Vue School/vue-router-4/vue-school-travel-app/src/router/index.js
+++ Vue School/vue-router-4/vue-school-travel-app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import sourceData from "@/data.json";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,20 @@ const router = createRouter({
       name: "destination.show",
       component: () => import("../views/DestinationShow.vue"),
       props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      beforeEnter(to, from) {
+        const exists = sourceData.destinations.find(
+          (destination) => destination.id === parseInt(to.params.id)
+        );
+        if (!exists)
+          return {
+            name: "NotFound",
+
+            // allows keeping the URL while rendering a different page
+            params: { pathMatch: to.path.split("/").slice(1) },
+            query: to.query,
+            hash: to.hash,
+          };
+      },
       children: [
         {
           path: ":experienceSlug",
