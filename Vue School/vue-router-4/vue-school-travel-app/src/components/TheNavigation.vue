a7af31873e59bcb1687065c6ea430a2fe08cc7d6
Chandan Ojha <chandanojha776@gmail.com>
2023-02-19 20:40:30 +0600

    Extending Router Link for External URLs

--- Vue School/vue-router-4/vue-school-travel-app/src/components/TheNavigation.vue
+++ Vue School/vue-router-4/vue-school-travel-app/src/components/TheNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link id="logo" to="/">Travel App</router-link>
-    <router-link
+    <AppLink id="logo" to="/">Travel App</AppLink>
+    <AppLink
       v-for="destination in destinations"
       :key="destination.id"
       :to="{
@@ -10,8 +10,9 @@
       }"
     >
       {{ destination.name }}
-    </router-link>
-    <router-link :to="{ name: 'protected' }">Dashboard</router-link>
+    </AppLink>
+    <AppLink :to="{ name: 'protected' }">Dashboard</AppLink>
+    <AppLink to="https://vueschool.io">Vue School</AppLink>
   </div>
 </template>
 
--- Vue School/vue-router-4/vue-school-travel-app/src/main.js
+++ Vue School/vue-router-4/vue-school-travel-app/src/main.js
@@ -3,12 +3,14 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import AppLink from "./components/AppLink.vue";
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.component("AppLink", AppLink);
 app.use(router);
 
 app.mount("#app");
