0b0d70899502c61da76a05402b55fbdd59c512c4
Chandan Ojha <chandanojha776@gmail.com>
2023-02-20 16:04:39 +0600

    Dynamic Routing

--- Vue School/vue-router-4/vue-school-travel-app/src/views/HomeView.vue
+++ Vue School/vue-router-4/vue-school-travel-app/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>All Destinations</h1>
-    <button @click="triggerRouterError">Trigger Router Errror</button>
+    <button @click="addDynamicRoute">Add Dynamic Route</button>
+    <router-link to="/dynamic">Visit Dynamic Route</router-link>
     <div class="destinations">
       <router-link
         v-for="destination in destinations"
@@ -29,18 +30,27 @@ export default {
     };
   },
   methods: {
-    async triggerRouterError() {
-      const navaigationFailure = await this.$router.push("/");
-      if (
-        isNavigationFailure(
-          navaigationFailure,
-          NavigationFailureType.duplicated
-        )
-      ) {
-        console.log(navaigationFailure.to);
-        console.log(navaigationFailure.from.fullPath);
-      } else {
-      }
+    // async triggerRouterError() {
+    //   const navaigationFailure = await this.$router.push("/");
+    //   if (
+    //     isNavigationFailure(
+    //       navaigationFailure,
+    //       NavigationFailureType.duplicated
+    //     )
+    //   ) {
+    //     console.log(navaigationFailure.to);
+    //     console.log(navaigationFailure.from.fullPath);
+    //   } else {
+    //   }
+    // },
+    addDynamicRoute() {
+      this.$router.addRoute({
+        name: "dynamic",
+        path: "/dynamic",
+        component: () => import("./Login.vue"),
+      });
+
+      this.$router.removeRoute("dynamic");
     },
   },
 };
